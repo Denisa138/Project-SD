e970d0eb339fe556712e6f2bdd25263c3123f349
Denisa138 <59956993+Denisa138@users.noreply.github.com>
2020-01-16 12:19:35 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include "eulerian_undirected_graph.h"
+#include "eulerian_directed_graph.h"
+
+int main()
+{
+	//cout << "\n---------Directed graph---------";
+	//test();
+
+	cout << "\n---------Undirected graph---------";
+	test2();
+	return 0;
+}
